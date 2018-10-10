877e47adab676193d03e2326012b27f6fb894e1a
Beppe Karlsson <beppe@live.se>
2018-10-10 21:28:30 +1100

    Create PageWrapper component

---
+++ src/layout/PageWrapper.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const PageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  margin-top: 30px;
+  min-height: calc(100vh - 150px);
+`;
+
+export default PageWrapper;
