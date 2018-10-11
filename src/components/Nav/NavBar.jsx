f681403bc5e7dfa9fe5b95d2704e1c03aac8b9d3
Beppe Karlsson <beppe@live.se>
2018-10-12 01:49:07 +1100

    Update navbar

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
+import { colors } from '../../constants/colors';
 
 const NavBar = styled.div`
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 0;
   width: 100%;
-  height: ${props => (props.height ? props.height : '50px')};
+  height: ${props => (props.height ? props.height : '60px')};
   text-align: center;
+  background-color: ${colors.background};
 `;
 
 export default NavBar;
--- src/layout/PageWrapper.jsx
+++ src/layout/PageWrapper.jsx
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
+import { colors } from '../constants/colors';
 
 const PageWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-content: center;
   align-items: center;
-  margin-top: 30px;
-  min-height: calc(100vh - 150px);
+  margin-top: 40px;
+  padding-top: 10px;
+  min-height: calc(100vh - 170px);
+  color: ${colors.text};
 `;
 
 export default PageWrapper;
