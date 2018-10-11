c0d3f661110afa7ed9a49a81e0c01975dd63fe28
Beppe Karlsson <beppe@live.se>
2018-10-12 01:47:43 +1100

    create colors constant

---
+++ src/constants/colors.js
@@ -0,0 +1,23 @@
+// http://paletton.com/#uid=13q0u0kSnBoreP7FRR8SIrlSZl4
+export const blueScale = {
+  superLight: '#29ABDC',
+  light: '#06A5E2',
+  medium: '#0282B3'
+};
+
+// http://paletton.com/#uid=1000u0k008i0gl201eR0I02a80s
+export const grayScale = {
+  superLigt: '#FFFEFE',
+  light: '#A8A6A6',
+  medium: '#767676',
+  dark: '#424242'
+};
+
+export const colors = {
+  text: grayScale.dark,
+  inactive: grayScale.light,
+  active: blueScale.medium,
+  hover: blueScale.light,
+  background: grayScale.superLigt,
+  link: blueScale.superLight
+};
