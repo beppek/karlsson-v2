9d382a7e0c5800179750c26058bb8bcfff5997e6
Beppe Karlsson <beppe@live.se>
2018-10-12 01:48:27 +1100

    Add Title and Subtitle

---
+++ src/commons/Title/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  text-align: center;
+`;
+
+export const Subtitle = styled.h3`
+  text-align: center;
+`;
+
+export default Title;
